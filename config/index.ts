62e4e9597a47455431ad56a888d54d1dfe77c299
saykat1 <nursyakat@gmail.com>
2021-08-10 12:27:39 +0600

    c

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 const dev:boolean =  process.env.NODE_ENV !== 'production';
 
-const rootURL:string = dev ? 'http://localhost:3000' : ''
+const rootURL:string = dev ? 'http://localhost:3000' : 'https://example.com'
 
 export default rootURL;
