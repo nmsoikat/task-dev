a7754a2380ea3d73557f86831b9bf0346faf2df5
saykat1 <nursyakat@gmail.com>
2021-08-10 12:29:38 +0600

    tring

--- pages/table.tsx
+++ pages/table.tsx
@@ -1,4 +1,4 @@
-import rootURL from "../config";
+// import rootURL from "../config";
 import Head from "next/head";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
