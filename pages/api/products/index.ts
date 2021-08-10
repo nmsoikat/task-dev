6cd818cad5c1b409111c4c27978d4e7b72f7c740
saykat1 <nursyakat@gmail.com>
2021-08-10 12:01:50 +0600

    api

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,9 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiResponse } from 'next'
 import {Products} from '../../../data/Products'
 
-export default function getProducts(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default function getProducts(res: NextApiResponse) {
   res.status(200).json(Products)
 }
